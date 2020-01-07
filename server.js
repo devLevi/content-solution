e1118a22d8d0d262671a39003b143ccb6003d3cf
Levi Bernard <levichristianbernard@gmail.com>
2020-01-06 21:58:26 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,45 @@
+'use-strict';
+const fs = require('fs'); // https://nodejs.org/api/fs.html
+const nodemailer = require('nodemailer');
+const mjml = require('mjml');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mjmlTemplate = fs.readFileSync('./email/index.mjml', 'utf8');
+const html = mjml(mjmlTemplate).html;
+
+app.use(express.static('public'));
+
+// Start the Express server
+app.listen(port, () => console.log(`Server running on port ${port}!`));
+
+const gmailTransporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'levichristianbernard@gmail.com',
+    pass: 'tebzobdudvmbqjfs'
+  }
+});
+
+const message = {
+  from: 'levichristianbernard@gmail.com', // Sender address
+  to: [
+    'lbernard.dev@gmail.com',
+    'lbernard.dev@yahoo.com',
+    'nadiabh182@gmail.com'
+    // 'homenowtest@gmail.com',
+    // 'homenowtest@outlook.com',
+    // 'homenowtest@yahoo.com',
+    // 'homenowtest@aol.com',
+    // 'homenowtest@homenow.io'
+  ], // List of recipients
+  subject: 'Home Now Safty Info | The Heimlich Maneuver', // Subject line
+  html: html
+};
+gmailTransporter.sendMail(message, function(err, info) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(info);
+  }
+});
